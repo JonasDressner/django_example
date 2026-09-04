0a3afbed5f5b75fba37403886a7d06de61067095
JonasDressner <jonas.dressner@gmx.de>
2026-09-04 06:36:30 +0200

    refactor: simplify Django entry point

--- manage.py
+++ manage.py
@@ -4,16 +4,12 @@
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+
 
 def main() -> None:
     """Run Django's command-line utility."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Django ist nicht installiert. Nutze: pip install -r requirements.txt"
-        ) from exc
     execute_from_command_line(sys.argv)
 
 
